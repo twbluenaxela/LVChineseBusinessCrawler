f6e3b19010c32bdea6416c57cbbec7e1ef70591a
Nick <rednaxela64@outlook.com>
2022-06-08 22:42:40 +0000

    Add constants and paths for web scrape
    The basic database format has now been setup. I added different
    field names so that I can be more consistent when adding new
    entries to the database.

---
+++ pachong/src/constants.js
@@ -0,0 +1,9 @@
+//export const BACKEND_BASE_URI = "http://localhost:3000";
+//export const COMPANIES_API_PATH = "/companies";
+
+// export const COMPANIES_TABLE_HEADERS = ["Company Name", "Status", "Created At", "Revenue YTD", "Account Executive"];
+
+export const COMPANY_CHINESE_FIELD_NAME = "chinese_name";
+export const COMPANY_ENGLISH_FIELD_NAME = "english_name";
+export const ADDRESS_FIELD_NAME = "address";
+export const PHONE_NUMBER_FIELD_NAME = "phone_number";
