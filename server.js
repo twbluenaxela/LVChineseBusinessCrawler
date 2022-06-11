3721ab01ee1832c9b67d8a5dcdf280332acee905
Nick <rednaxela64@outlook.com>
2022-06-11 04:00:56 +0000

    Fix commit issues

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const cors = require('cors');
 const webScraper = require('./webscraper');
 const { Console } = require('console');
+// console.log(typeof webScraper.webScraper())
 // Create an Express application
 const app = express();
 
@@ -12,16 +13,17 @@ const app = express();
 const port = process.env.PORT || 3001;
 app.set('port', port);
 
+
+const corsOption = {
+    origin: ['http://localhost:3001'],
+};
+
+
 // Load middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-
-
-const corsOption = {
-  origin: ['http://localhost:3001'],
-};
 
 
 // Have Node serve the files for our built React app
@@ -34,15 +36,15 @@ app.get("/api", (req, res) => {
 
 app.get('/favicon.ico', (req, res) => res.status(204).end());
 
-app.post("/api/scrape", async (req, res) => {
+app.post("/scrape", async (req, res) => {
   console.log("Got the request!")
   // const JSONString = JSON.stringify(req.body)
   console.log(req.body.url)
   let urlToSendBack = req.body.url
-  let fetchedResults = await webScraper.webScraper(urlToSendBack)
+  let fetchedResults = webScraper.webScraper(urlToSendBack)
   console.log("Sending it back!")
   console.log(fetchedResults)
-  res.send(fetchedResults)
+  res.send({data: fetchedResults})
 }
 )
 
